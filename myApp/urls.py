2ae76c28e3c9f02145cfe4e02e53a9745141d0f0
raguldavid1999 <raguldavid1999@gmail.com>
2024-04-26 23:15:37 +0530

    add require

--- myApp/urls.py
+++ myApp/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('sample',views.sampleAPI.as_view())
 ]
--- myApp/views.py
+++ myApp/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 def home(request):
     context = {}
     return render(request, 'myAppTemp/index.html',context)
+
+class sampleAPI(APIView):
+    def get(self, request):
+        return Response([
+            {
+                'name': 'Abhishek',
+                'age': 20
+            }
+        ])
